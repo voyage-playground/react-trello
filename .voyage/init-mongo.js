16e4fca639688ad73311de291caa1ef2cf5e0fdd
Jaden Lemmon <jaden@jadenlemmon.com>
2021-09-08 15:38:56 -0500

    Polish off voyage integration (#1)
    * update db image
    
    * Update
    
    * add redis
    
    * init script
    
    * update redis
    
    * Update env
    
    * update init script
    
    * add diagram

--- .voyage/init-mongo.js
+++ .voyage/init-mongo.js
@@ -23,18 +23,44 @@ db.boards.insert([
         label: '2/2',
         cards: [
           {
-            id: 'Card1',
-            title: 'Write Blog',
-            description: 'Can AI make memes',
-            label: '30 mins',
-            draggable: false,
+            id: 1,
+            title: 'Create new logo',
+            description:
+              'The logo is like 5 years old and we need a new one fast!',
+            label: '12/7/2020',
           },
           {
-            id: 'Card2',
-            title: 'Pay Rent',
-            description: 'Transfer via NEFT',
-            label: '5 mins',
-            metadata: { sha: 'be312a1' },
+            id: 2,
+            title: 'Login button bug',
+            description: 'The login button is acting odd and flashing weird.',
+            label: '3/5/2021',
+          },
+          {
+            id: 3,
+            title: 'Mobile friendly',
+            description:
+              'The UI interface is not mobile friendly and we should take a deep dive into this.',
+            label: '6/6/2021',
+          },
+          {
+            id: 4,
+            title: 'Remove HTML blink tags',
+            description: 'Why are we using stuff from the 90s?',
+            label: '9/24/2020',
+          },
+          {
+            id: 5,
+            title: 'Use the new color scheme',
+            description:
+              'Marketing has set out a new color scheme we need to adopt on the web.',
+            label: '10/12/2020',
+          },
+          {
+            id: 6,
+            title: 'Remove all food images',
+            description:
+              'We should not have pictures of food on our website as it makes people too hungry.',
+            label: '8/4/2021',
           },
         ],
       },
@@ -42,25 +68,109 @@ db.boards.insert([
         id: 'lane2',
         title: 'Selected for Development',
         label: '0/0',
-        cards: [],
+        cards: [
+          {
+            id: 1,
+            title: 'Create a diagram',
+            description:
+              'We should have an architecture diagram for the entire app processes.',
+            label: '9/25/2020',
+          },
+          {
+            id: 2,
+            title: 'E2E tests',
+            description: 'Consider using cypress for E2E tests.',
+            label: '3/12/2021',
+          },
+          {
+            id: 3,
+            title: 'Stop the background music',
+            description:
+              'This is not myspace. We should not play background music and annoy people.',
+            label: '12/16/2020',
+          },
+          {
+            id: 4,
+            title: 'Add coupon code box',
+            description:
+              'Users should be able to add a coupon code to get a discount.',
+            label: '6/29/2021',
+          },
+          {
+            id: 5,
+            title: 'Add steal your data checkbox',
+            description:
+              'We should autocheck a checkbox upon signup to let users know we will steal their data.',
+            label: '8/17/2021',
+          },
+        ],
       },
       {
         id: 'lane3',
         title: 'In Progress',
         label: '0/0',
-        cards: [],
+        cards: [
+          {
+            id: 1,
+            title: 'Nav height',
+            description:
+              'The nav is way to tall and does not meet design spec.',
+            label: '2/18/2021',
+          },
+          {
+            id: 2,
+            title: 'Integrate Voyage',
+            description:
+              'We need to integrate Voyage for PR preview deployments.',
+            label: '5/25/2021',
+          },
+        ],
       },
       {
         id: 'lane4',
         title: 'In QA',
         label: '0/0',
-        cards: [],
+        cards: [
+          {
+            id: 1,
+            title: 'Password Reset',
+            description: 'Allow users to reset their password.',
+            label: '1/9/2021',
+          },
+          {
+            id: 2,
+            title: 'Setup an actual DB',
+            description:
+              'Move off of microsoft access to a real DB. Maybe SQL server.',
+            label: '8/15/2021',
+          },
+        ],
       },
       {
         id: 'lane5',
         title: 'Shipped',
         label: '0/0',
-        cards: [],
+        cards: [
+          {
+            id: 1,
+            title: 'Pay Us Page',
+            description:
+              'Create a page where users can pay us 100 dollars with one click for no reason.',
+            label: '9/25/2020',
+          },
+          {
+            id: 2,
+            title: 'Video Chat',
+            description: 'Allow users to video chat with customer support.',
+            label: '7/13/2021',
+          },
+          {
+            id: 3,
+            title: 'Sell flamethrowers',
+            description: 'Pull an Elon and sell random stuff.',
+            label: '11/10/2020',
+          },
+        ],
       },
     ],
   },
--- README.md
+++ README.md
@@ -1,70 +1,24 @@
-# Getting Started with Create React App
+# Trello Clone Voyage Example App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+## Running
 
-In the project directory, you can run:
+```
+npm i
+npm start
+```
 
-### `yarn start`
+## About
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+This is a simple Trello clone application. The board is pre-populated with tasks. If you alter the board all changes are logged as an activity log to a redis instance. All new tickets trigger a GCP Pub/Sub message that in turn triggers a cloud function to deliver a slack notification. This application is backed by a Mongo DB.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+Voyage has been integrated into this application in the `.voyage` folder. This repositiory is acting as the "primary" trigger for deploying Voyage meaning that pushes to this repo will trigger re-builds.
 
-### `yarn test`
+This application is configured to deploy it's backend API from https://github.com/voyage-playground/react-trello-api.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+This application utlizes isolated containers deployed via Voyage and connects to some external GCP services. GCP auth is made possible via a service account that is set via an environment variable in the Voyage UI and then mapped to a file in the API container startup script.
 
-### `yarn build`
+## Application Architecture Diagram
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+![Diagram](/diagram.png)
