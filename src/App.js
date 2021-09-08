9311e829f3b3f357a12bf427aca47de10a8852c8
Jaden Lemmon <jaden@jadenlemmon.com>
2021-09-07 20:51:03 -0500

    Add env

--- src/App.js
+++ src/App.js
@@ -45,66 +45,20 @@ const GlobalStyle = createGlobalStyle(
   `
 );
 
-const data = {
-  lanes: [
-    {
-      id: 'lane1',
-      title: 'Backlog',
-      label: '2/2',
-      cards: [
-        {
-          id: 'Card1',
-          title: 'Write Blog',
-          description: 'Can AI make memes',
-          label: '30 mins',
-          draggable: false,
-        },
-        {
-          id: 'Card2',
-          title: 'Pay Rent',
-          description: 'Transfer via NEFT',
-          label: '5 mins',
-          metadata: { sha: 'be312a1' },
-        },
-      ],
-    },
-    {
-      id: 'lane2',
-      title: 'Selected for Development',
-      label: '0/0',
-      cards: [],
-    },
-    {
-      id: 'lane3',
-      title: 'In Progress',
-      label: '0/0',
-      cards: [],
-    },
-    {
-      id: 'lane4',
-      title: 'In QA',
-      label: '0/0',
-      cards: [],
-    },
-    {
-      id: 'lane5',
-      title: 'Shipped',
-      label: '0/0',
-      cards: [],
-    },
-  ],
-};
-
 const StyledBoard = styled(Board)`
   height: 100%;
   background: none;
 `;
 
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_URL,
+});
+
 const App = () => {
   const [lanes, setLanes] = useState({ lanes: [] });
 
   const fetch = async () => {
-    const { data } = await axios.get('http://localhost:8080/board');
+    const { data } = await instance.get('/board');
     setLanes(data);
   };
 
@@ -113,7 +67,7 @@ const App = () => {
   }, []);
 
   const handleUpdate = async (data) => {
-    await axios.post('http://localhost:8080/board', data);
+    await instance.post('/board', data);
   };
 
   return (
